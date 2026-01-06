ac27fa967597529c066a54b379a52452a647983e
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2026-01-06 17:46:42 +0500

    checking

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <CartProvider>
-          <ProductProvider>
-          <MenuProvider>
+        {/* <CartProvider> */}
+          {/* <ProductProvider> */}
+          {/* <MenuProvider> */}
         <App />
-        </MenuProvider>
-        </ProductProvider>
-        </CartProvider>
+        {/* </MenuProvider> */}
+        {/* </ProductProvider> */}
+        {/* </CartProvider> */}
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,9 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoutes from '../pages/Auth/Routes';
 import DashboardRoutes from '../pages/Dashboard/Routes';
-import AddProductForm from '../components/AddProducts/AddProductForm';
-import { MenuManagement } from '../views/MenuManagement';
-import { EditItemPage } from '../views/EditItemPage';
+// import AddProductForm from '../components/AddProducts/AddProductForm';
+// import { MenuManagement g} from '../views/MenuManagement';
+// import { EditItemPage } from '../views/EditItemPage';
 // import { useCallback, useEffect, useState } from 'react';
 // import { DealGallery } from '../components/MenuItems/DealGallery';
 // import { ProductSelection } from '../components/MenuItems/ProductSelection';
@@ -13,12 +13,12 @@ import { EditItemPage } from '../views/EditItemPage';
 import Navbar from '../components/Header/Navbar';
 // import axiosInstance from '../api/axiosInstance';
 import HomePage from './../pages/HomePage';
-import CartPage from '../pages/CartPage';
+// import CartPage from '../pages/CartPage';
 import NotFound from '../pages/Frontend/NotFound';
 import AnalyticsDashboard from '../pages/Frontend/AnalyticsDashboard';
-import { MenuItemsPage } from '../views/MenuItemPage';
+// import { MenuItemsPage } from '../views/MenuItemPage';
 import { useAuth } from '../context/useAuthContext';
-import OrdersPage from '../pages/Frontend/Orders';
+// import OrdersPage from '../pages/Frontend/Orders';
 
 const AppRoutes = () => {
   const { role} = useAuth();
@@ -29,7 +29,7 @@ const AppRoutes = () => {
       <Routes>
         {/* === PUBLIC ROUTES (Everyone) === */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<CartPage />} />
+        {/* <Route path="/cart" element={<CartPage />} /> */}
         <Route path="/auth/*" element={<AuthRoutes />} />
 
         {/* === PROTECTED ROUTES === */}
@@ -45,50 +45,50 @@ const AppRoutes = () => {
         />
 
         {/* 2. MENU ITEMS: Super Admin & Cashier */}
-        <Route
+        {/* <Route
           path="/menu-items"
           element={
             <ProtectedRoute allowedRoles={['superadmin', 'cashier']}>
               <MenuItemsPage />
             </ProtectedRoute>
           }
-        />
+        /> */}
 
         {/* 3. EDIT ITEMS: Super Admin & Cashier */}
-        <Route
+        {/* <Route
           path="/edit-item/:id"
           element={
             <ProtectedRoute allowedRoles={['superadmin', 'cashier']}>
               <EditItemPage />
             </ProtectedRoute>
           }
-        />
+        /> */}
 
         {/* 4. ADD PRODUCTS: Super Admin ONLY */}
-        <Route
+        {/* <Route
           path="/add-products"
           element={
             <ProtectedRoute allowedRoles={['superadmin']}>
               <AddProductForm />
             </ProtectedRoute>
           }
-        />
+        /> */}
 
         {/* 5. MENU MANAGEMENT: Super Admin ONLY */}
-        <Route
+        {/* <Route
           path="/menu-management"
           element={
             <ProtectedRoute allowedRoles={['superadmin']}>
               <MenuManagement />
             </ProtectedRoute>
           }
-        />
+        /> */}
         {/* <Route path="orders" element={<Cart />} /> */}
 
         {/* 404 */}
         <Route path="*" element={<NotFound />} />
         <Route path="/analytic" element={<AnalyticsDashboard />} />
-        <Route path='/orders' element={<OrdersPage/>}/>
+        {/* <Route path='/orders' element={<OrdersPage/>}/> */}
       </Routes>
     </>
   );
